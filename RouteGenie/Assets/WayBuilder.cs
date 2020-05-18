3c7367f004cb26a74ba4f1e6f97a5d681f949c85
kasper <kasperhangard@hotmail.com>
2020-05-18 10:07:16 +0200

    Init Commit

---
+++ RouteGenie/Assets/WayBuilder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WayBuilder : MonoBehaviour
+{
+    public int NodesToDraw = 0;
+    public bool DrawAll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DrawMap());
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void DrawWay(Way way)
+    {
+
+        List<Node> nodes = new List<Node>();// XMLReader.Nodes.Where(node => way.Nodes.Contains(node.ID)).ToList();
+        
+        foreach(long id in way.Nodes)
+        {
+            nodes.Add(XMLReader.Nodes.Find(node => node.ID == id));
+        }
+
+        foreach(long id in way.Nodes.Except(nodes.Select(node => node.ID))){
+            Debug.Log("missing: " + id);
+        }
+        
+
+
+        for (int i = 0; i < (DrawAll ? nodes.Count-1 : NodesToDraw); i++)
+        {
+            Debug.DrawLine(CreatePos(nodes[i]), CreatePos(nodes[i+1]), Color.red, 10);
+        }
+    }
+
+    IEnumerator DrawMap()
+    {
+        while (true)
+        {
+
+            if(XMLReader.Ways.Count > 0)
+            {
+                foreach (Way way in XMLReader.Ways)
+                {
+                    yield return new WaitForSeconds(5 / XMLReader.Ways.Count);
+                    DrawWay(way);
+                }
+            } else
+            {
+                yield return new WaitForSeconds(1);
+            }
+
+
+        }
+    }
+
+    Vector2 CreatePos (Node node)
+    {
+        Vector2 MyPos = new Vector2(9.981820f, 57.016701f);
+        return (MyPos - new Vector2((float)node.Lon, (float)node.Lat)) * 10000;
+    }
+}
---
+++ RouteGenie/Assets/XMLReader.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System;
+
+public class XMLReader : MonoBehaviour
+{
+
+    public static List<Node> Nodes = new List<Node>();
+    public static List<Way> Ways = new List<Way>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        XmlTextReader reader = new XmlTextReader("C:\\Users\\kaspe\\Desktop\\Roads.xml");
+
+        Way way = new Way();
+        while (reader.Read())
+        {
+            if (reader.Name == "node")
+            {
+                Node node = new Node();
+                try { 
+                    while (reader.MoveToNextAttribute())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "id":
+                                node.ID = long.Parse(reader.Value);
+                                break;
+                            case "lat":
+                                node.Lat = double.Parse(reader.Value);
+                                break;
+                            case "lon":
+                                node.Lon = double.Parse(reader.Value);
+                                break;
+                        }
+
+                    }
+                } catch(Exception ex)
+                {
+                    Debug.Log("tried to convert: " + reader.Name + " " + reader.Value + ". " + ex);
+                }
+                Nodes.Add(node);
+            }
+
+            if (reader.Name == "way")
+            {
+                way = new Way();
+                try
+                {
+                    while (reader.MoveToNextAttribute())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "id":
+                                way.ID = long.Parse(reader.Value);
+                                break;
+                        }
+
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log("tried to convert: " + reader.Name + " " + reader.Value + ". " + ex);
+                }
+                Ways.Add(way);
+            }
+
+            if (reader.Name == "nd")
+            {
+                try
+                {
+                    while (reader.MoveToNextAttribute())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "ref":
+                                way.Nodes.Add(long.Parse(reader.Value));
+                                break;
+                        }
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log("tried to convert: " + reader.Name + " " + reader.Value + ". " + ex);
+                }
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+public class Node
+{
+    public long ID;
+    public double Lon, Lat;
+
+    public override string ToString()
+    {
+        return ID.ToString();
+    }
+}
+
+public class Way
+{
+    public enum RoadType
+    {
+        foot, bike, car
+    }
+
+    public long ID;
+    public List<long> Nodes;
+    RoadType Type;
+
+    public Way()
+    {
+        Nodes = new List<long>();
+    }
+}
